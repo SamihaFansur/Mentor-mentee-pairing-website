1fbf4825c669594310f653a9edb653f2a17c8ed2
Yuting Du <ydu54@sheffield.ac.uk>
2021-03-04 17:20:52 +0000

    Update user.rb

--- models/user.rb
+++ models/user.rb
@@ -1,5 +1,5 @@
-# A user record from the database
-class User < Sequel::Model
+# A mentee record from the database
+class mentee < Sequel::Model
   def name
     "#{first_name} #{surname}"
   end
@@ -7,7 +7,7 @@ class User < Sequel::Model
   def self.id_exists?(id)
     return false if id.nil? # check the id is exists
     return false unless Validation.str_is_integer?(id) # check the id is an integer
-    return false if User[id].nil? # check the database has a record with this id
+    return false if mentee[id].nil? # check the database has a record with this id
 
     true # all checks are right
   end
@@ -19,11 +19,10 @@ class User < Sequel::Model
     self.phone_number = params.fetch("phone_number", "").strip
     self.course = params.fetch("course", "").strip
     self.course_years = params.fetch("course_years", "").strip
-    self.position = params.fetch("position", "").strip
-    self.signal = params.fetch("signal", "").strip
-    self.criteria = params.fetch("criteria", "").strip
+    self.university = params.fetch("university", "").strip
+    
   end
-# users' imformation would be saved and displayed
+# mentees' imformation would be saved and displayed
   def validate
     super
     errors.add("first_name", "cannot be empty") if !first_name || first_name.empty?
@@ -32,8 +31,7 @@ class User < Sequel::Model
     errors.add("phone_number", "cannot be empty") if !phone_number || phone_number.empty?
     errors.add("course", "cannot be empty") if !course || course.empty?
     errors.add("course_years", "cannot be empty") if !course_years || course_years.empty?
-    errors.add("position", "cannot be empty") if !position || position.empty?
-    errors.add("signal", "cannot be empty") if !signal|| signal.empty?
-    errors.add("criteria", "cannot be empty") if !criteria || criteria.empty?
+    errors.add("university", "cannot be empty") if !university || university.empty?
+    
 end
-# If users forget to finish all the questions, there has some warnings to them
+# If mentees forget to finish all the questions, there has some warnings to them
