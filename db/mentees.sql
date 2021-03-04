9e669c8b42ff48e545385998ac2c802251a583e4
Yuting Du <ydu54@sheffield.ac.uk>
2021-03-04 14:51:55 +0000

    Update mentees.sql

---
+++ db/mentees.sql
@@ -0,0 +1,16 @@
+CREATE TABLE mentees (
+    id INTEGER PRIMARY KEY,
+    first_name TEXT,
+    surname TEXT,
+    email TEXT,
+    phone_number INTEGER,
+    course TEXT,
+    course_year TEXT,
+    position TEXT,
+);
+
+INSERT INTO mentees VALUES();
+
+SELECT * FROM mentees WHERE id = 1;
+
+DELETE FROM mentees WHERE position = "";
