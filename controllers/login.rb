ce6c510ed187f7010181d4999161c8759ae678b7
Samiha Fansur <sfansur1@sheffield.ac.uk>
2021-03-10 23:50:21 +0000

    SUCCESSFUL MENTOR AND MENTEE LOGIN/SIGNUP

--- controllers/login.rb
+++ controllers/login.rb
@@ -1,4 +1,3 @@
-#MENTEE
 get "/" do
   redirect "/login" unless session[:logged_in]
   erb :dashboard
@@ -6,16 +5,33 @@ end
 
 get "/login" do
   @mentees = Mentee.new
+  
+  @mentors = Mentee.new
   erb :login
 end
 
 post '/login' do
   @mentees = Mentee.new
   @mentees.load(params)
+  
+  @mentors = Mentor.new
+  @mentors.load(params)
+  
   @error = nil
 
   if @mentees.valid?
     if @mentees.exist?
+      session[:logged_in] = true
+      redirect "/"
+    else
+      @error = "Username/Password combination incorrect"
+    end
+  else
+    @error = "Please correct the information below"
+  end
+  
+  if @mentors.valid?
+    if @mentors.exist?
       session[:logged_in] = true
       redirect "/"
     else
@@ -32,38 +48,3 @@ get "/logout" do
   session.clear
   erb :logout
 end
-
-#MENTOR------------NEED TO FIGURE OUT HOW TO LOGIN BOTH MENTOR AND MENTEE AT SAME TIME
-# get "/" do
-#   redirect "/login" unless session[:logged_in]
-#   erb :dashboard
-# end
-
-# get "/login" do
-#   @mentors = Mentee.new
-#   erb :login
-# end
-
-# post '/login' do
-#   @mentors = Mentor.new
-#   @mentors.load(params)
-#   @error = nil
-
-#   if @mentors.valid?
-#     if @mentors.exist?
-#       session[:logged_in] = true
-#       redirect "/"
-#     else
-#       @error = "Username/Password combination incorrect"
-#     end
-#   else
-#     @error = "Please correct the information below"
-#   end
-
-#   erb :login
-# end
-
-# get "/logout" do
-#   session.clear
-#   erb :logout
-# end
