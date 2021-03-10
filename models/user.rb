6e92a2acb2641963639159d99cbd0766d4ab9c0c
Yuting Du <ydu54@sheffield.ac.uk>
2021-03-10 16:38:16 +0000

    Add new file

---
+++ models/user.rb
@@ -0,0 +1,20 @@
+require "openssl"
+
+# A user record from the database
+class User < Sequel::Model
+  def load(params)
+    self.username = params.fetch("username", "").strip
+    self.password = params.fetch("password", "").strip
+  end
+
+  def validate
+    super
+    errors.add("username", "cannot be empty") if username.empty?
+    errors.add("password", "cannot be empty") if password.empty?
+  end
+
+  def exist?
+    other_user = User.first(username: username)
+    !other_user.nil? && other_user.password == password
+  end
+end
