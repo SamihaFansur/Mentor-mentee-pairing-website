aacd46600490e07c5064e1f42206da46b4c4ac44
Euan Goodbrand <egoodbrand1@sheffield.ac.uk>
2021-03-16 12:09:27 +0000

    changed validation.rb to return a ture value when mentee is not nil

--- helpers/validation.rb
+++ helpers/validation.rb
@@ -1,8 +1,8 @@
 # Provide various functions for validating data
 module Validation
- def self.str_is_text?(str)
+ def self.str_is_text?(str) 
     return false if str.nil?
-
+    return true if !str.nil?
 #     str.match?(/^(\d)+$/)
   end
 
