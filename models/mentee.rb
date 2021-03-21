cf4e313e60fd9344b7759cecede83e1e67f6fe9b
Euan Goodbrand <egoodbrand1@sheffield.ac.uk>
2021-03-21 18:58:14 +0000

    edited mentee and mentor models

--- models/mentee.rb
+++ models/mentee.rb
@@ -8,10 +8,10 @@ class Mentee < Sequel::Model
 
   # "self.method" is how we define a class-level method in Ruby (in the same way
   # we'd use "static" in Java, e.g., public static void classMethod(...))
-  def self.username_exists?(username)
-   return false if username.nil? # check the username is not nil
-   return false unless Validation.str_is_text?(username) # check the username is text
-   return false if Mentee[username].nil? # check the database has a record with this username
+  def self.id_exists?(id)
+   return false if id.nil? # check the username is not nil
+   return false unless Validation.str_is_text?(id) # check the username is text
+   return false if Mentee[id].nil? # check the database has a record with this username
 
    true # all checks are ok - the username exists
   end
@@ -28,6 +28,7 @@ class Mentee < Sequel::Model
   end
   
   def loadEdit(params)
+    self.id = params.fetch("id", self.id).strip
     self.fname = params.fetch("fname", self.fname).strip
     self.lname = params.fetch("lname", self.lname).strip
     self.email = params.fetch("email", self.email).strip
