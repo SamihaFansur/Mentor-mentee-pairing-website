e01cdcd53b4f2b20bd5a8b8c6e537fe55f0da3b5
Euan Goodbrand <egoodbrand1@sheffield.ac.uk>
2021-03-21 17:07:20 +0000

    added new spec/features folder for acceptance tests and added add_spec.rb

---
+++ spec/features/add_spec.rb
@@ -0,0 +1,10 @@
+require_relative "../spec_helper"
+
+describe "the signup page" do
+    it "is accessible from the search page" do
+        visit"/index"
+        click_link "Sign Up"
+        expect(page).to have_content "Mentee"
+    end
+
+end
