8a5e0238d05eb565da1f3f802a738266cd35447b
Euan Goodbrand <egoodbrand1@sheffield.ac.uk>
2021-03-22 13:36:42 +0000

    added more unit tests for mentee_spec and mentor_spec and added comments

--- spec/unit/mentee_spec.rb
+++ spec/unit/mentee_spec.rb
@@ -1,12 +1,40 @@
 require_relative "../spec_helper"
 
 RSpec.describe Mentee do
+ #checks that name returns correctly from database
   describe "#name" do
     it "returns the mentee's full name" do
       mentee = described_class.new(fname: "test", lname: "test")
       expect(mentee.name).to eq("test test")
     end
   end
-
+ #checks that email returns correctly from database  
+  describe "#email" do
+    it "returns the mentee's email" do
+      mentee = described_class.new(email: "test@gmail.com")
+      expect(mentee.email).to eq("test@gmail.com")
+    end
+  end
+#checks that phoneNum returns correctly from database
+      describe "#phoneNum" do
+    it "returns the mentee's full phoneNum" do
+      mentee = described_class.new(phoneNum: "123456789")
+      expect(mentee.phoneNum).to eq("123456789")
+    end
+  end
+#checks that username returns correctly from database
+      describe "#username" do
+    it "returns the mentee's username" do
+      mentee = described_class.new(username: "1234")
+      expect(mentee.username).to eq("1234")
+    end
+  end
+#checks that password returns correctly from database
+      describe "#password" do
+    it "returns the mentee's password" do
+      mentee = described_class.new(password: "1234")
+      expect(mentee.password).to eq("1234")
+    end
+  end
 
 end
--- spec/unit/mentor_spec.rb
+++ spec/unit/mentor_spec.rb
@@ -1,12 +1,39 @@
 require_relative "../spec_helper"
 
 RSpec.describe Mentor do
+    #checks that name returns correctly from database
   describe "#name" do
-    it "returns the mentee's full name" do
+    it "returns the mentor's full name" do
       mentor = described_class.new(fname: "test", lname: "test")
       expect(mentor.name).to eq("test test")
     end
   end
-
-
+#checks that email returns correctly from database
+ describe "#email" do
+    it "returns the mentor's email" do
+      mentor = described_class.new(email: "test@gmail.com")
+      expect(mentor.email).to eq("test@gmail.com")
+    end
+  end
+#checks that phoneNum returns correctly from database
+      describe "#phoneNum" do
+    it "returns the mentor's full phoneNum" do
+      mentor = described_class.new(phoneNum: "123456789")
+      expect(mentor.phoneNum).to eq("123456789")
+    end
+  end
+#checks that username returns correctly from database
+      describe "#username" do
+    it "returns the mentor's username" do
+      mentor = described_class.new(username: "1234")
+      expect(mentor.username).to eq("1234")
+    end
+  end
+#checks that password returns correctly from database
+      describe "#password" do
+    it "returns the mentor's password" do
+      mentor = described_class.new(password: "1234")
+      expect(mentor.password).to eq("1234")
+    end
+  end
 end
