554f4a97a9f943da99e071c8e02da1eb655fd64a
Euan Goodbrand <egoodbrand1@sheffield.ac.uk>
2021-03-22 13:02:49 +0000

    edited add_spec.rb to include tests for mentors

--- spec/features/add_spec.rb
+++ spec/features/add_spec.rb
@@ -1,19 +1,19 @@
 require_relative "../spec_helper"
-
+#checks that signup is accessible from /index and that mentee signup is inside
 describe "the signup page" do
     it "is accessible from the search page" do
         visit"/index"
         click_link "Sign Up"
         expect(page).to have_content "Mentee"
     end
-    
+    #tests that an empty signup form wont submit for mentee
     it "will not add a mentee with no details" do
         visit "/MenteeSignUpForm"
         click_button "Submit"
         expect(page).to have_content "Mentee Sign Up"
     end
-    
-    it "adds a player into the fields" do
+    #adds a mentee and tests that they are added
+    it "adds a mentee into the fields" do
         visit "/MenteeSignUpForm"
         add_test_user
         visit "/login"
@@ -24,4 +24,32 @@ describe "the signup page" do
         expect(page).to have_content "George Test"
         clear_database
     end
+    
+    
+    
+    #checks that signup is accessible from /index and that mentor signup is inside
+        it "is accessible from the search page" do
+        visit"/index"
+        click_link "Sign Up"
+        expect(page).to have_content "Mentor"
+    end
+    #tests that an empty signup form wont submit for mentor
+    it "will not add a mentor with no details" do
+        visit "/MentorSignUpForm"
+        click_button "Submit"
+        expect(page).to have_content "Mentor Sign Up"
+    end
+    #adds a mentor and tests that they are added
+    it "adds a mentor into the fields" do
+        add_test_mentor
+        visit "/login"
+        fill_in "username", with: "1234"
+        fill_in "password", with: "1234"
+        click_button "Submit"
+        visit"/MentorDashboard"
+        expect(page).to have_content "Sam Mentor"
+        clear_database
+    end
+    
+    
 end
