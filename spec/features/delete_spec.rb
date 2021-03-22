e0094ad4387658906016dc6bf9ceafb7571da9d2
Euan Goodbrand <egoodbrand1@sheffield.ac.uk>
2021-03-22 12:50:27 +0000

    updated delete_spec to include mentor test

--- spec/features/delete_spec.rb
+++ spec/features/delete_spec.rb
@@ -13,4 +13,17 @@ describe "the delete page" do
     expect(page).to have_content "Unknown mentee"
     clear_database
   end
+    
+      it "allows deletion of a valid mentee" do
+    add_test_mentor
+    visit "/login"
+    fill_in "username", with: "1234"
+    fill_in "password", with: "1234"
+    click_button "Submit"
+    visit "/editMentor?id=1"
+    click_button "Delete"
+    visit "/editMentor?id=1"
+    expect(page).to have_content "Unknown mentor"
+    clear_database
+  end
 end
