a4fb8affba6c2e78c64eef5d7088277ee3029320
Samiha Fansur <sfansur1@sheffield.ac.uk>
2021-03-22 15:02:20 +0000

    Added comments in requests.rb

--- models/requests.rb
+++ models/requests.rb
@@ -10,6 +10,7 @@ class Request < Sequel::Model
   
   #Checks if application already sent to prevent multiple submissions
   def exist_application?
+    #Goes through all the records in requests table and returns true if mentor id is present
     other_request = Request.where(menteeID: menteeID)
     other_request.each do |requestVar|
      return true if requestVar.mentorID == mentorID
