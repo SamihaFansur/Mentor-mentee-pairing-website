a75726400322aba8916c5caf2562ef6b4fe51c34
Euan Goodbrand <egoodbrand1@sheffield.ac.uk>
2021-03-22 12:47:46 +0000

    edited edit_spec to include tests for mentors

--- spec/features/edit_spec.rb
+++ spec/features/edit_spec.rb
@@ -46,4 +46,49 @@ describe "the edit page" do
     expect(page).to have_content "Test"
     clear_database
   end
+    
+    
+    
+    
+    
+    it "allows editing of a valid player" do
+    add_test_mentor
+    visit "/login"
+    fill_in "username", with: "1234"
+    fill_in "password", with: "1234"
+    click_button "Submit"
+    visit "/editMentor?id=1"
+    click_button "Submit"
+    visit "/MentorDashboard"
+    expect(page).to have_content "Sam Mentor"
+    clear_database
+  end
+
+  it "allows a player record to be changed" do
+    add_test_mentor
+    visit "/login"
+    fill_in "username", with: "1234"
+    fill_in "password", with: "1234"
+    click_button "Submit"
+    visit "/editMentor?id=1"
+    fill_in "fname", with: "New"
+    click_button "Submit"
+    visit "/MentorDashboard"
+    expect(page).to have_content "New"
+    clear_database
+  end
+
+  it "will save empty data field" do
+    add_test_mentor
+    visit "/login"
+    fill_in "username", with: "1234"
+    fill_in "password", with: "1234"
+    click_button "Submit"
+    visit "/editMentor?id=1"
+    fill_in "fname", with: ""
+    click_button "Submit"
+    visit"/MentorDashboard"
+    expect(page).to have_content "Mentor"
+    clear_database
+  end
 end
