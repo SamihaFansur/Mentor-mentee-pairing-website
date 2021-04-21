2e09150942241c88b5f4ea0dca0bb476bbe6528c
Euan Goodbrand <egoodbrand1@sheffield.ac.uk>
2021-04-21 12:38:09 +0000

    added edit tests for admin+mentor profiles

--- spec/features/edit_spec.rb
+++ spec/features/edit_spec.rb
@@ -161,7 +161,72 @@ describe "the admin edit page" do
   end    
 end
     
+ #checks that a admin+mentor can be edited
+describe "the admin edit page" do
     
+  it "shows an error when trying to load an invalid mentee" do
+    visit "/editAdmin?id=1000000"
+    expect(page).to have_no_xpath('a')
+  end    
+    
+    it "allows editing of a valid mentor" do
+    add_test_mentor
+    visit "/login"
+    fill_in "username", with: "1234"
+    fill_in "password", with: "1234"    
+    click_button "Submit" 
+    visit "/login"
+    fill_in "username", with: "admin2"
+    fill_in "password", with: "S.F"    
+    click_button "Submit"  
+    visit "/editMentor?id=1"
+    click_button "Submit"
+    visit "/AdminMentorDashboard"
+    expect(page).to have_content "Sam"
+    clear_database
+  end
+#checks that mentor edit is submitted and displayed
+  it "allows a mentor record to be changed" do
+    add_test_mentor
+    visit "/login"
+    fill_in "username", with: "1234"
+    fill_in "password", with: "1234"    
+    click_button "Submit" 
+    visit "/login"
+    fill_in "username", with: "admin2"
+    fill_in "password", with: "S.F"    
+    click_button "Submit"
+    visit "/editMentor?id=1"
+    fill_in "fname", with: "New"
+    click_button "Submit"
+    visit "/AdminMentorDashboard"
+    expect(page).to have_content "New"
+    clear_database
+  end
+    
+#checks that an empty data field can be submitted for mentor
+  it "will save empty data field" do
+    add_test_mentor
+    visit "/login"
+    fill_in "username", with: "1234"
+    fill_in "password", with: "1234"    
+    click_button "Submit" 
+    visit "/login"
+    fill_in "username", with: "admin2"
+    fill_in "password", with: "S.F"    
+    click_button "Submit"
+    visit "/editMentor?id=1"
+    fill_in "fname", with: ""
+    click_button "Submit"
+    visit "/AdminMentorDashboard"
+    expect(page).to have_content "Mentor"
+    visit "/editAdmin?id=2"
+    fill_in "fname", with: "Sam"
+    click_button "Submit"
+    clear_database
+  end    
+end   
+
     
     clear_database
 end
