b30b368fe56c61c0df87434ffc59d73142c5b8a5
Euan Goodbrand <egoodbrand1@sheffield.ac.uk>
2021-04-25 17:42:00 +0000

    added tests for admin and admin+mentors deleting mentors and mentees

--- spec/features/delete_spec.rb
+++ spec/features/delete_spec.rb
@@ -26,5 +26,109 @@ describe "the delete page" do
     expect(page).to have_content "one-to-one mentoring program"
     clear_database
   end
+    
+   it "tests for a mentor delete by admin" do
+  clear_database
+    add_test_mentor
+    add_test_user
+    visit "/login" 
+    fill_in "username", with: "123"
+    fill_in "password", with: "123"
+    click_button "Submit"      
+    visit "/login"
+    fill_in "username", with: "admin2"
+    fill_in "password", with: "S.F"
+    click_button "Submit"
+    click_link "No"
+    click_link "Mentee List"
+    click_link "Edit Profile"
+    click_button "Delete"
+    visit "/login"
+    fill_in "username", with: "123"
+    fill_in "password", with: "123"
+    click_button "Submit"
+    expect(page).to have_content "Username/Password combination incorrect"
+
+    clear_database
+  end
+ it "tests for a mentee delete by admin " do
+ clear_database
+    add_test_mentor
+    add_test_user
+    visit "/login" 
+    fill_in "username", with: "1234"
+    fill_in "password", with: "1234"
+    click_button "Submit"      
+    visit "/login"
+    fill_in "username", with: "admin2"
+    fill_in "password", with: "S.F"
+    click_button "Submit"
+    click_link "No"
+    click_link "Mentor List"
+    click_link "Edit Profile"
+    click_button "Delete"
+    visit "/login"
+    fill_in "username", with: "1234"
+    fill_in "password", with: "1234"
+    click_button "Submit"
+    expect(page).to have_content "Username/Password combination incorrect"
+
+    clear_database
+ end
+        
+   it "tests for a mentor delete by admin mentor" do
+  clear_database
+    add_test_mentor
+    add_test_user
+    visit "/login" 
+    fill_in "username", with: "123"
+    fill_in "password", with: "123"
+    click_button "Submit"      
+    visit "/login"
+    fill_in "username", with: "admin2"
+    fill_in "password", with: "S.F"
+    click_button "Submit"
+    click_link "Yes"
+    fill_in "username", with: "1234"
+    fill_in "password", with: "1234"
+    click_button "Submit"   
+    click_link "Mentee List"
+    click_link "Edit Profile"
+    click_button "Delete"
+    visit "/login"
+    fill_in "username", with: "123"
+    fill_in "password", with: "123"
+    click_button "Submit"
+    expect(page).to have_content "Username/Password combination incorrect"
+
+    clear_database
+  end
+ it "tests for a mentee delete by admin mentor" do
+ clear_database
+    add_test_mentor
+    add_test_user
+    visit "/login" 
+    fill_in "username", with: "1234"
+    fill_in "password", with: "1234"
+    click_button "Submit"      
+    visit "/login"
+    fill_in "username", with: "admin2"
+    fill_in "password", with: "S.F"
+    click_button "Submit"
+    click_link "Yes"
+    fill_in "username", with: "1234"
+    fill_in "password", with: "1234"
+    click_button "Submit"   
+    click_link "Mentor List"
+    click_link "Edit Profile"
+    click_button "Delete"
+    visit "/login"
+    fill_in "username", with: "1234"
+    fill_in "password", with: "1234"
+    click_button "Submit"
+    expect(page).to have_content "Username/Password combination incorrect"
+
+    clear_database
+ end
     clear_database
 end
