7da7e08f3811cbb0364545d8bef6867c4a4a20f5
Samiha Fansur <sfansur1@sheffield.ac.uk>
2021-04-25 22:39:58 +0000

    refactored mainPages.rb controller

--- controllers/mainPages.rb
+++ controllers/mainPages.rb
@@ -1,71 +1,15 @@
 get "/" do
-  @header = nil #initializing header 
-  
-  #If user already logged in then displays a header for profiles which are logged 
-  #in else displays headers for non-logged in users
-  if session[:logged_in]
-    if session[:admins_username]
-      if session[:mentors_username]
-        @header = erb:"common/header_adminMentorA"
-      else
-        @header = erb:"common/header_adminA"
-      end
-    elsif session[:mentees_username] 
-      @header = erb:"common/header_menteeA"
-    elsif session[:mentors_username] 
-      @header = erb:"common/header_mentorA"
-    end
-  else 
-    @header = erb:"common/headerB"
-  end
-  
+  headers_common_pages
   erb :index
 end
 
 get "/contact" do
-   @header = nil #initializing header
-  
-  #If user already logged in then displays a header for profiles which are logged 
-  if session[:logged_in]
-    if session[:admins_username]
-      if session[:mentors_username]
-        @header = erb:"common/header_adminMentorA"
-      else
-        @header = erb:"common/header_adminA"
-      end
-    elsif session[:mentees_username] 
-      @header = erb:"common/header_menteeA"
-    elsif session[:mentors_username] 
-      @header = erb:"common/header_mentorA"
-    end
-  else 
-    @header = erb:"common/headerB"
-  end
-  
+  headers_common_pages  
   erb :contact
 end
 
 get "/accessibility" do
-   @header = nil #initializing header
-  
-  #If user already logged in then displays a header for profiles which are logged 
-  #in else displays headers for non-logged in users
-  if session[:logged_in]
-    if session[:admins_username]
-      if session[:mentors_username]
-        @header = erb:"common/header_adminMentorA"
-      else
-        @header = erb:"common/header_adminA"
-      end
-    elsif session[:mentees_username] 
-      @header = erb:"common/header_menteeA"
-    elsif session[:mentors_username] 
-      @header = erb:"common/header_mentorA"
-    end
-  else 
-    @header = erb:"common/headerB"
-  end
-  
+  headers_common_pages  
   erb :accessibility
 end
 
@@ -119,4 +63,25 @@ end
 post "/MenteeSchedule" do
  @header = erb:"common/header_mentorA"
   erb :mentee_schedule
+end
+
+def headers_common_pages
+  #If user already logged in then displays a header for profiles which are logged 
+  #in else displays headers for non-logged in users
+  if session[:logged_in]
+    if session[:admins_username]
+      if session[:mentors_username]
+        @header = erb:"common/header_adminMentorA"
+      else
+        @header = erb:"common/header_adminA"
+      end
+    elsif session[:mentees_username] 
+      @header = erb:"common/header_menteeA"
+    elsif session[:mentors_username] 
+      @header = erb:"common/header_mentorA"
+    end
+  else 
+    @header = erb:"common/headerB"
+  end
+  
 end
