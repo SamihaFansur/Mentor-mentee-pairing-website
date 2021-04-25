b2f87272533834a86ed66db13d6fce9d3f8695a1
Euan Goodbrand <egoodbrand1@sheffield.ac.uk>
2021-04-25 17:04:22 +0000

    added new block_spec tests

--- spec/features/block_spec.rb
+++ spec/features/block_spec.rb
@@ -305,5 +305,167 @@ it "shows empty list message when no mentors" do
     expect(page).to have_content "Your account is suspended"
     clear_database
  end
+   it "allows block and unblock of a valid mentee" do
+    add_test_user
+   add_test_mentor
+    visit "/login"
+    fill_in "username", with: "admin2"
+    fill_in "password", with: "S.F"
+    click_button "Submit"
+    click_link "Yes"
+    fill_in "username", with: "1234"
+    fill_in "password", with: "1234"
+    click_button "Submit"
+    click_link "Mentee List"
+    click_button "Block"
+    click_button "Unblock"
+    visit "/login"
+    fill_in "username", with: "123"
+    fill_in "password", with: "123"
+    click_button "Submit"
+    expect(page).to have_content "George Test"
+    clear_database
+  end
+       
+  it "allows block and unblock of a valid mentor" do
+    add_test_mentor
+    visit "/login"
+    fill_in "username", with: "1234"
+    fill_in "password", with: "1234"
+    click_button "Submit"
+    add_test_mentor
+    visit "/login"
+    fill_in "username", with: "admin2"
+    fill_in "password", with: "S.F"
+    click_button "Submit"
+    click_link "Yes"
+    fill_in "username", with: "1234"
+    fill_in "password", with: "1234"
+    click_button "Submit"
+    click_link "Mentor List"
+    click_button "Block"
+    click_button "Unblock"
+    visit "/login"
+    fill_in "username", with: "1234"
+    fill_in "password", with: "1234"
+    click_button "Submit"
+    expect(page).to have_content "Sam Mentor"
+    clear_database
+  end 
+ it "tests for a mentee/mentor match" do
+  clear_database
+    add_test_mentor
+    add_test_user
+    visit "/login" 
+    fill_in "username", with: "1234"
+    fill_in "password", with: "1234"
+    click_button "Submit"      
+    visit "/login" 
+    fill_in "username", with: "123"
+    fill_in "password", with: "123"
+    click_button "Submit"
+    visit "/search"
+    fill_in "searchForMentor", with: "Accounting and Financial Management (BA)"
+    click_button "Submit"
+    click_button "Send application" 
+    visit "/login"
+    fill_in "username", with: "1234"
+    fill_in "password", with: "1234"
+    click_button "Submit"
+    visit "/menteeApplications"
+
+    click_button "Accept application"
+    visit "/login"
+    fill_in "username", with: "admin2"
+    fill_in "password", with: "S.F"
+    click_button "Submit"
+    click_link "No"
+    click_link "Mentor List"
+    click_button "Block"
+      visit "/login"
+    fill_in "username", with: "1234"
+    fill_in "password", with: "1234"
+    click_button "Submit"
+    expect(page).to have_content "Your account is suspended"
+    clear_database
+  end
+ it "tests for a mentee/mentor match" do
+  clear_database
+    add_test_mentor
+    add_test_user
+    visit "/login" 
+    fill_in "username", with: "1234"
+    fill_in "password", with: "1234"
+    click_button "Submit"      
+    visit "/login" 
+    fill_in "username", with: "123"
+    fill_in "password", with: "123"
+    click_button "Submit"
+    visit "/search"
+    fill_in "searchForMentor", with: "Accounting and Financial Management (BA)"
+    click_button "Submit"
+    click_button "Send application" 
+    visit "/login"
+    fill_in "username", with: "1234"
+    fill_in "password", with: "1234"
+    click_button "Submit"
+    visit "/menteeApplications"
+
+    click_button "Accept application"
+    visit "/login"
+    fill_in "username", with: "admin2"
+    fill_in "password", with: "S.F"
+    click_button "Submit"
+    click_link "Yes"
+    fill_in "username", with: "1234"
+    fill_in "password", with: "1234"
+    click_button "Submit"
+    click_link "Mentor List"
+    click_button "Block"
+    visit "/login"
+    fill_in "username", with: "1234"
+    fill_in "password", with: "1234"
+    click_button "Submit"
+    expect(page).to have_content "Your account is suspended"
+    clear_database
+ end
+ it "tests for a mentee/mentor match" do
+  clear_database
+    add_test_mentor
+    add_test_user
+    visit "/login" 
+    fill_in "username", with: "1234"
+    fill_in "password", with: "1234"
+    click_button "Submit"      
+    visit "/login" 
+    fill_in "username", with: "123"
+    fill_in "password", with: "123"
+    click_button "Submit"
+    visit "/search"
+    fill_in "searchForMentor", with: "Accounting and Financial Management (BA)"
+    click_button "Submit"
+    click_button "Send application" 
+    visit "/login"
+    fill_in "username", with: "1234"
+    fill_in "password", with: "1234"
+    click_button "Submit"
+    visit "/menteeApplications"
+
+    click_button "Accept application"
+    visit "/login"
+    fill_in "username", with: "admin2"
+    fill_in "password", with: "S.F"
+    click_button "Submit"
+    click_link "No"
+    click_link "Mentor List"
+    click_button "Block"
+    visit "/login"
+    fill_in "username", with: "1234"
+    fill_in "password", with: "1234"
+    click_button "Submit"
+    expect(page).to have_content "Your account is suspended"
+    clear_database
+  end
+
         clear_database
 end
