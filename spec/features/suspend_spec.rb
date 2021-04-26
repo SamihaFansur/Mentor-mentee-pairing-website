35bcb60cf7f03cd258933c776b952c8e89604344
Euan Goodbrand <egoodbrand1@sheffield.ac.uk>
2021-04-26 00:44:24 +0000

    added test for time taken after a suspension to be unsuspended

--- spec/features/suspend_spec.rb
+++ spec/features/suspend_spec.rb
@@ -257,5 +257,44 @@ it "allows suspend of a valid mentee but not twice" do
     expect(page).to have_content "Account has already been unsuspended"
     clear_database
   end 
+   it "shows empty list message when no mentors" do
+    add_test_user
+    visit "/login"
+    fill_in "username", with: "admin2"
+    fill_in "password", with: "S.F"
+    click_button "Submit"
+    click_link "No"
+    click_link "Mentee List"
+    click_button "Suspend"
+    sleep(31)
+    visit "/login"
+    fill_in "username", with: "123"
+    fill_in "password", with: "123"
+    click_button "Submit"
+    expect(page).to have_content "George Test"
+    clear_database
+  end
+       
+  it "allows editing of a valid mentee" do
+    add_test_mentor
+    visit "/login"
+    fill_in "username", with: "1234"
+    fill_in "password", with: "1234"
+    click_button "Submit"
+    visit "/login"
+    fill_in "username", with: "admin2"
+    fill_in "password", with: "S.F"
+    click_button "Submit"
+    click_link "No"
+    click_link "Mentor List"
+    click_button "Suspend"
+    sleep(31)
+    visit "/login"
+    fill_in "username", with: "1234"
+    fill_in "password", with: "1234"
+    click_button "Submit"
+    expect(page).to have_content "Sam Mentor"
+    clear_database
+  end 
         clear_database
 end
