41ff0394dec2cd5f4d307e5847fc4eccae2bed27
Euan Goodbrand <egoodbrand1@sheffield.ac.uk>
2021-04-26 17:37:56 +0000

    added test for sending applicatoin when already have mentor

--- spec/features/match_spec.rb
+++ spec/features/match_spec.rb
@@ -56,6 +56,62 @@ context "with an empty database" do
     expect(page).to have_content "Sam Mentor"
     clear_database
   end
+      it "tests for a mentee/mentor match" do
+  clear_database
+      add_test_mentor
+      add_test_user
+      visit "/login" 
+      fill_in "username", with: "1234"
+      fill_in "password", with: "1234"
+      click_button "Submit"      
+      visit "/login" 
+      fill_in "username", with: "123"
+      fill_in "password", with: "123"
+      click_button "Submit"
+      visit "/search"
+      fill_in "searchForMentor", with: "Accounting and Financial Management (BA)"
+      click_button "Submit"
+      click_button "Send application" 
+    visit "/login"
+    fill_in "username", with: "1234"
+    fill_in "password", with: "1234"
+    click_button "Submit"
+    visit "/menteeApplications"
+
+    click_button "Accept application"
+    visit "/login"
+    fill_in "username", with: "123"
+    fill_in "password", with: "123"
+    click_button "Submit"
+    visit "/MenteeDashboard"
+    click_link "View mentor applications"
+    click_button "Match"
+    visit "/MenteeDashboard"
+    click_link "My Mentor"
+    expect(page).to have_content "Sam Mentor"
+    visit "/MentorSignUpForm"
+    fill_in "fname", with: "Sam"
+    fill_in "lname", with: "Mentor"
+    fill_in "email", with: "arb21eg@sheffield.ac.uk"
+    fill_in "phoneNum", with: "+44 7721851137"
+    fill_in "username", with: "12345"
+    fill_in "password", with: "12345"
+    select "Aerospace Engineering with a Year in North America (Meng)", :from => "courseName"
+    select "Account Collector", :from => "jobTitle"
+    click_button "Submit"        
+    visit "/login" 
+    fill_in "username", with: "12345"
+    fill_in "password", with: "12345"
+    click_button "Submit"      
+    visit "/login" 
+    fill_in "username", with: "123"
+    fill_in "password", with: "123"
+    click_button "Submit"
+    visit "/search?error=2" 
+    expect(page).to have_content "You already have a mentor"
+    clear_database
+  end
+    
 end
     clear_database
 end
