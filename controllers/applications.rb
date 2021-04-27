646a7a6afbe31fbaa6305a51ded5cccf056e8f96
Euan Goodbrand <egoodbrand1@sheffield.ac.uk>
2021-04-27 12:19:43 +0000

    removed unused code, removed schedule

--- controllers/applications.rb
+++ controllers/applications.rb
@@ -31,17 +31,6 @@ get "/sentMentorApplications" do
 end
 
 
-post "/match" do    
-  #When mentor accepts mentee request, mentorMatch field updated to value of mentor ID in the mentees table
-  Mentee.where(id: params[:menteeID]).update(:mentorMatch => $mentors.id)
-  
-  #When mentor accepts mentee request, menteeMatch field updated to value of mentee ID in the mentors table
-  Mentor.where(id: $mentors.id).update(:menteeMatch => params[:menteeID])
-  
-  #Deletes the all requests a mentor has from the table after mentor accepts a mentee request
-  Request.where(mentorID: $mentors.id).delete
-  redirect "/MentorDashboard"
-end
 
 #Mentee accepts mentor
 post "/matchWithMentor" do
@@ -279,32 +268,6 @@ get "/myMentee" do
   
   erb :myMentee
 end
-
-get "/myMenteeSchedule" do
-  #List to store the mentee matched to a mentor
- @menteeMatchedList = []
-  #Finds the ID of the mentee that is equal to the value stored in the menteeMatch column in the mentors table
-  MenteeIDList = Mentee.where(id: $mentors.menteeMatch)
-  MenteeIDList.each do |id|
-    @menteeMatchedList.push(Mentee.first(id: id.id)) #Stores the mentor record found in the list to be displayed
-  end
-  
-  erb :mentee_schedule
-end
-
-
-get "/myMentorSchedule" do
-  #List to store the mentee matched to a mentor
- @mentorMatchedList = []
-  #Finds the ID of the mentor that is equal to the value stored in the mentorMatch column in the mentees table
-  MentorIDList = Mentor.where(id: $mentees.mentorMatch)
-  MentorIDList.each do |id|
-    @mentorMatchedList.push(Mentor.first(id: id.id)) #Stores the mentee record found in the list to be displayed
-  end
-  
-  erb :mentor_schedule
-end
-
 
 #Route for the admin to view pending mentee applications
 get "/PendingMenteeApplications" do
