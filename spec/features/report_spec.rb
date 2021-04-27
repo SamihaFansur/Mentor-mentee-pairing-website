a1dd8aebf9958a1dd80b00a75e7ae8ffb7fd7a8e
Euan Goodbrand <egoodbrand1@sheffield.ac.uk>
2021-04-27 13:52:05 +0000

    refactored and commented report_spec.rb

--- spec/features/report_spec.rb
+++ spec/features/report_spec.rb
@@ -2,71 +2,46 @@ require_relative "../spec_helper"
 
 
     
-#checks that a mentee can be edited
-#
-#
-#tests that error message shows up when a nil mentee is attempted to be edited
-describe "applications page" do
-context "with an empty database" do
+#checks that a mentor can send reports
+
+describe "mentor report" do
+context "with a mentee" do
 
        
-  it "tests for a mentee/mentor match" do
+  it "tests the mentors can report their mentee" do
   clear_database
-      add_test_mentor
-      add_test_user
-      visit "/login" 
-      fill_in "username", with: "1234"
-      fill_in "password", with: "1234"
-      click_button "Submit"      
-      visit "/login" 
-      fill_in "username", with: "123"
-      fill_in "password", with: "123"
-      click_button "Submit"
-      visit "/search"
-      fill_in "searchForMentor", with: "Accounting and Financial Management (BA)"
-      click_button "Submit"
-      click_button "Send application" 
-    visit "/login"
-    fill_in "username", with: "1234"
-    fill_in "password", with: "1234"
+    add_test_mentor
+    add_test_user
+    login_mentor     
+    login_mentee
+    visit "/search"
+    fill_in "searchForMentor", with: "Accounting and Financial Management (BA)"
     click_button "Submit"
+    click_button "Send application" 
+    login_mentor   
     visit "/menteeApplications"
-
     click_button "Accept application"
-    visit "/login"
-    fill_in "username", with: "123"
-    fill_in "password", with: "123"
-    click_button "Submit"
+    login_mentee
     visit "/MenteeDashboard"
     click_link "View mentor applications"
     click_button "Match"
     visit "/MenteeDashboard"
     click_link "My Mentor"
     expect(page).to have_content "Sam Mentor"
-    visit "/login"
-    fill_in "username", with: "1234"
-    fill_in "password", with: "1234"
-    click_button "Submit"
+    login_mentor   
     click_link "My Mentee"
     click_button "Report Mentee"
-
     select "Public Shaming", :from => "caption"
     fill_in "Description:", with: "spam"
     click_button "Submit"
-    visit "/login"
-    fill_in "username", with: "admin2"
-    fill_in "password", with: "S.F"
-    click_button "Submit"
-    click_link "No"
+    login_admin
     click_link "Reports"
     click_button "View"
     expect(page).to have_content "spam"
     
     clear_database
   end
-
-
-    
+  
 end
     clear_database
 end
