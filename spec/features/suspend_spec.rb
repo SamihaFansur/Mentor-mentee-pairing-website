19aeba56e75339b90a79fca0caa201421a5e868e
Euan Goodbrand <egoodbrand1@sheffield.ac.uk>
2021-04-27 14:00:30 +0000

    refactored suspend_spec.rb (finished refactoring for spec/features)

--- spec/features/suspend_spec.rb
+++ spec/features/suspend_spec.rb
@@ -9,99 +9,57 @@ require_relative "../spec_helper"
 
 describe "admin suspend page" do
   it "shows empty list message when no mentors" do
-    visit "/login"
-    fill_in "username", with: "admin2"
-    fill_in "password", with: "S.F"
-    click_button "Submit"
-    click_link "No"
+    login_admin
     click_link "Mentee List"
     expect(page).to have_content "Your search revealed no mentees."
     clear_database
   end
        
   it "allows editing of a valid mentee" do
-    visit "/login"
-    fill_in "username", with: "admin2"
-    fill_in "password", with: "S.F"
-    click_button "Submit"
-    click_link "No"
+    login_admin
     click_link "Mentor List"
     expect(page).to have_content "Your search revealed no mentors."
     clear_database
   end
    it "shows empty list message when no mentors" do
     add_test_user
-    visit "/login"
-    fill_in "username", with: "admin2"
-    fill_in "password", with: "S.F"
-    click_button "Submit"
-    click_link "No"
+    login_admin
     click_link "Mentee List"
     click_button "Suspend"
-    visit "/login"
-    fill_in "username", with: "123"
-    fill_in "password", with: "123"
-    click_button "Submit"
+    login_mentee
     expect(page).to have_content "Your account is suspended"
     clear_database
   end
        
   it "allows editing of a valid mentee" do
     add_test_mentor
-    visit "/login"
-    fill_in "username", with: "1234"
-    fill_in "password", with: "1234"
-    click_button "Submit"
-    visit "/login"
-    fill_in "username", with: "admin2"
-    fill_in "password", with: "S.F"
-    click_button "Submit"
-    click_link "No"
+    login_mentor
+    login_admin
     click_link "Mentor List"
     click_button "Suspend"
-    visit "/login"
-    fill_in "username", with: "1234"
-    fill_in "password", with: "1234"
-    click_button "Submit"
+    login_mentor
     expect(page).to have_content "Your account is suspended"
     clear_database
   end
    it "shows empty list message when no mentors" do
     add_test_user
-    visit "/login"
-    fill_in "username", with: "admin2"
-    fill_in "password", with: "S.F"
-    click_button "Submit"
-    click_link "No"
+    login_admin
     click_link "Mentee List"
     click_button "Suspend"
     click_button "Unsuspend"
-    visit "/login"
-    fill_in "username", with: "123"
-    fill_in "password", with: "123"
-    click_button "Submit"
+    login_mentee
     expect(page).to have_content "George Test"
     clear_database
   end
        
   it "allows editing of a valid mentee" do
     add_test_mentor
-    visit "/login"
-    fill_in "username", with: "1234"
-    fill_in "password", with: "1234"
-    click_button "Submit"
-    visit "/login"
-    fill_in "username", with: "admin2"
-    fill_in "password", with: "S.F"
-    click_button "Submit"
-    click_link "No"
+    login_mentor
+    login_admin
     click_link "Mentor List"
     click_button "Suspend"
     click_button "Unsuspend"
-    visit "/login"
-    fill_in "username", with: "1234"
-    fill_in "password", with: "1234"
-    click_button "Submit"
+    login_mentor
     expect(page).to have_content "Sam Mentor"
     clear_database
   end 
@@ -109,36 +67,19 @@ describe "admin suspend page" do
   clear_database
     add_test_mentor
     add_test_user
-    visit "/login" 
-    fill_in "username", with: "1234"
-    fill_in "password", with: "1234"
-    click_button "Submit"      
-    visit "/login" 
-    fill_in "username", with: "123"
-    fill_in "password", with: "123"
-    click_button "Submit"
+    login_mentor      
+    login_mentee
     visit "/search"
     fill_in "searchForMentor", with: "Accounting and Financial Management (BA)"
     click_button "Submit"
     click_button "Send application" 
-    visit "/login"
-    fill_in "username", with: "1234"
-    fill_in "password", with: "1234"
-    click_button "Submit"
+    login_mentor
     visit "/menteeApplications"
-
     click_button "Accept application"
-    visit "/login"
-    fill_in "username", with: "admin2"
-    fill_in "password", with: "S.F"
-    click_button "Submit"
-    click_link "No"
+    login_admin
     click_link "Mentee List"
     click_button "Suspend"
-    visit "/login"
-    fill_in "username", with: "123"
-    fill_in "password", with: "123"
-    click_button "Submit"
+    login_mentee
     expect(page).to have_content "Your account is suspended"
     clear_database
   end
@@ -146,54 +87,27 @@ it "tests for a mentee/mentor match" do
   clear_database
     add_test_mentor
     add_test_user
-    visit "/login" 
-    fill_in "username", with: "1234"
-    fill_in "password", with: "1234"
-    click_button "Submit"      
-    visit "/login" 
-    fill_in "username", with: "123"
-    fill_in "password", with: "123"
-    click_button "Submit"
+    login_mentor     
+    login_mentee
     visit "/search"
     fill_in "searchForMentor", with: "Accounting and Financial Management (BA)"
     click_button "Submit"
     click_button "Send application" 
-    visit "/login"
-    fill_in "username", with: "1234"
-    fill_in "password", with: "1234"
-    click_button "Submit"
+    login_mentor
     visit "/menteeApplications"
-
     click_button "Accept application"
-    visit "/login"
-    fill_in "username", with: "admin2"
-    fill_in "password", with: "S.F"
-    click_button "Submit"
-    click_link "No"
+    login_admin
     click_link "Mentor List"
     click_button "Suspend"
-    visit "/login"
-    fill_in "username", with: "1234"
-    fill_in "password", with: "1234"
-    click_button "Submit"
+    login_mentor
     expect(page).to have_content "Your account is suspended"
     clear_database
   end
   it "allows suspend of a valid mentor but not twice" do
     add_test_mentor
-    visit "/login"
-    fill_in "username", with: "1234"
-    fill_in "password", with: "1234"
-    click_button "Submit"
+    login_mentor
     add_test_mentor
-    visit "/login"
-    fill_in "username", with: "admin2"
-    fill_in "password", with: "S.F"
-    click_button "Submit"
-    click_link "Yes"
-    fill_in "username", with: "1234"
-    fill_in "password", with: "1234"
-    click_button "Submit"
+    login_admin_mentor
     click_link "Mentor List"
     click_button "Suspend"
     click_button "Suspend"
@@ -204,19 +118,9 @@ it "tests for a mentee/mentor match" do
 it "allows suspend of a valid mentee but not twice" do
     add_test_user
     add_test_mentor
-    visit "/login"
-    fill_in "username", with: "1234"
-    fill_in "password", with: "1234"
-    click_button "Submit"
+    login_mentor
     add_test_mentor
-    visit "/login"
-    fill_in "username", with: "admin2"
-    fill_in "password", with: "S.F"
-    click_button "Submit"
-    click_link "Yes"
-    fill_in "username", with: "1234"
-    fill_in "password", with: "1234"
-    click_button "Submit"
+    login_admin_mentor
     click_link "Mentee List"
     click_button "Suspend"
     click_button "Suspend"
@@ -226,11 +130,7 @@ it "allows suspend of a valid mentee but not twice" do
   end
    it "shows empty list message when no mentors" do
     add_test_user
-    visit "/login"
-    fill_in "username", with: "admin2"
-    fill_in "password", with: "S.F"
-    click_button "Submit"
-    click_link "No"
+    login_admin
     click_link "Mentee List"
     click_button "Suspend"
     click_button "Unsuspend"
@@ -241,15 +141,8 @@ it "allows suspend of a valid mentee but not twice" do
        
   it "allows editing of a valid mentee" do
     add_test_mentor
-    visit "/login"
-    fill_in "username", with: "1234"
-    fill_in "password", with: "1234"
-    click_button "Submit"
-    visit "/login"
-    fill_in "username", with: "admin2"
-    fill_in "password", with: "S.F"
-    click_button "Submit"
-    click_link "No"
+    login_mentor
+    login_admin
     click_link "Mentor List"
     click_button "Suspend"
     click_button "Unsuspend"
@@ -259,40 +152,23 @@ it "allows suspend of a valid mentee but not twice" do
   end 
    it "shows empty list message when no mentors" do
     add_test_user
-    visit "/login"
-    fill_in "username", with: "admin2"
-    fill_in "password", with: "S.F"
-    click_button "Submit"
-    click_link "No"
+    login_admin
     click_link "Mentee List"
     click_button "Suspend"
     sleep(31)
-    visit "/login"
-    fill_in "username", with: "123"
-    fill_in "password", with: "123"
-    click_button "Submit"
+    login_mentee
     expect(page).to have_content "George Test"
     clear_database
   end
        
   it "allows editing of a valid mentee" do
     add_test_mentor
-    visit "/login"
-    fill_in "username", with: "1234"
-    fill_in "password", with: "1234"
-    click_button "Submit"
-    visit "/login"
-    fill_in "username", with: "admin2"
-    fill_in "password", with: "S.F"
-    click_button "Submit"
-    click_link "No"
+    login_mentor
+    login_admin
     click_link "Mentor List"
     click_button "Suspend"
     sleep(31)
-    visit "/login"
-    fill_in "username", with: "1234"
-    fill_in "password", with: "1234"
-    click_button "Submit"
+    login_mentor
     expect(page).to have_content "Sam Mentor"
     clear_database
   end 
