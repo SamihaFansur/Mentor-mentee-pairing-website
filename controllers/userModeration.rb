8ed5bf29b103b1858132185c19047bf74f683eab
Samiha Fansur <sfansur1@sheffield.ac.uk>
2021-04-28 02:58:47 +0000

    unblock user routes refactored, AND userModeration.rb refactored completely

--- controllers/userModeration.rb
+++ controllers/userModeration.rb
@@ -41,23 +41,9 @@ end
 post "/unblockMentee" do
   @mentees = Mentee.first(id: params[:menteeID]) #creates a new instance of mentee
   
-  #Sets suspendMentee field to 0 to indicate account is unblocked
-  if @mentees.suspendMentee == 1
-    @mentees.suspendMentee = 0
-    @mentees.save_changes
-    #Sends mentee an email that their account has been unsuspended
-    send_mail(@mentees.email, 
-        "Mentee account Unblocked!", 
-        "Hi "+@mentees.fname+" "+@mentees.lname+" !\n"+
-        "Your mentee account has now been unblocked.\n"+
-        "You can now login using your mentee credentials"+
-        "\n\nRegards\nTeam 6")
-   else
-      @error1 ="Account has already been unblocked"
-      redirect "/searchForAMentee?error=4"
-  end
+  unblock_user(@mentees, @mentees.suspendMentee)
     
-    redirect "/searchForAMentee"
+  redirect "/searchForAMentee"
 end
 
 post "/blockMentor" do
@@ -71,23 +57,9 @@ end
 post "/unblockMentor" do
   @mentors = Mentor.first(id: params[:mentorID]) #creates a new instance of mentor
 
-  #Sets suspendMentor field to 0 to indicate account unsuspended if its already suspended
-  if @mentors.suspendMentor == 1
-    @mentors.suspendMentor = 0
-    @mentors.save_changes
-    #Sends mentor an email that their account has been unsuspended
-    send_mail(@mentors.email, 
-        "Mentor account Unblocked!", 
-        "Hi "+@mentors.fname+" "+@mentors.lname+" !\n"+
-        "Your mentor account has now been unblocked.\n"+
-        "You can now login using your mentor credentials"+
-        "\n\nRegards\nTeam 6")
-   else
-      @error1 ="Account has already been unblocked"
-      redirect "/searchForAMentor?error=4"
-  end
+  unblock_user(@mentors, @mentors.suspendMentor)
     
-    redirect "/searchForAMentor"
+  redirect "/searchForAMentor"
 end
 
 def suspend_user(user, suspendUserField)
@@ -124,11 +96,11 @@ def suspend_user(user, suspendUserField)
             "\n\nRegards\nTeam 6")
       } 
    else
-      if user.class == Mentee
-        redirect "/searchForAMentee?error=1"
-      elsif user.class == Mentor
-        redirect "/searchForAMentor?error=1"
-      end
+    if user.class == Mentee
+      redirect "/searchForAMentee?error=1"
+    elsif user.class == Mentor
+      redirect "/searchForAMentor?error=1"
+    end
   end
 end
 
@@ -148,11 +120,11 @@ def unsuspend_user(user, unsuspendUserField)
         "You can now login using your credentials"+
         "\n\nRegards\nTeam 6")
    else
-      if user.class == Mentee
-        redirect "/searchForAMentee?error=2"
-      elsif user.class == Mentor
-        redirect "/searchForAMentor?error=2"
-      end
+    if user.class == Mentee
+      redirect "/searchForAMentee?error=2"
+    elsif user.class == Mentor
+      redirect "/searchForAMentor?error=2"
+    end
   end
 end
 
@@ -173,10 +145,35 @@ def block_user(user, blockUserField)
         "If you think we have made a mistake, please contact our admins using our contact form!"+
         "\n\nRegards\nTeam 6")
   else
-      if user.class == Mentee
-        redirect "/searchForAMentee?error=3"
-      elsif user.class == Mentor
-        redirect "/searchForAMentor?error=3"
-      end
+    if user.class == Mentee
+      redirect "/searchForAMentee?error=3"
+    elsif user.class == Mentor
+      redirect "/searchForAMentor?error=3"
+    end
+  end
+end
+
+def unblock_user(user, unblockUserField)
+  #Sets unsuspendUserField field to 0 to indicate account unsuspended permanently/unblocked
+  if unblockUserField == 1
+    if user.class == Mentee
+      user.suspendMentee = 0
+    elsif user.class == Mentor
+      user.suspendMentor = 0
+    end
+    user.save_changes
+    #Sends user an email that their account has been unblocked
+    send_mail(user.email, 
+        "Account Unblocked!", 
+        "Hi "+user.fname+" "+user.lname+" !\n"+
+        "Your account has now been unblocked.\n"+
+        "You can now login using your credentials"+
+        "\n\nRegards\nTeam 6")
+   else
+    if user.class == Mentee
+      redirect "/searchForAMentee?error=4"
+    elsif user.class == Mentor
+      redirect "/searchForAMentor?error=4"
+    end
   end
 end
