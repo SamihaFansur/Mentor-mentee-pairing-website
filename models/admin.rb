7e9eaca9779ca019fa3ee8a98f92955bb4e375ca
Oliver Cowell <ocowell1@sheffield.ac.uk>
2021-04-29 20:20:03 +0000

    minor refactoring of id_exists method in the models

--- models/admin.rb
+++ models/admin.rb
@@ -11,10 +11,12 @@ class Admin < Sequel::Model
   # "self.method" is how we define a class-level method in Ruby (in the same way
   # we'd use "static" in Java, e.g., public static void classMethod(...))
   def self.id_exists?(id)
-   return false if id.nil? # check the id is not nil
-   return false unless Validation.str_is_text?(id) # check the id is text
-   return false if Admin[id].nil? # check the database has a record with this id
-
+   if id.nil? || !Validation.str_is_text?(id) || Mentee[id].nil?
+     return false
+   end
+   # check the id is not nil
+   # check the id is text
+   # check the database has a record with this id
    true # all checks are ok - the id exists
   end
 
--- models/mentee.rb
+++ models/mentee.rb
@@ -11,11 +11,10 @@ class Mentee < Sequel::Model
   # "self.method" is how we define a class-level method in Ruby (in the same way
   # we'd use "static" in Java, e.g., public static void classMethod(...))
   def self.id_exists?(id)
-   return false if id.nil? # check the id is not nil
-   return false unless Validation.str_is_text?(id) # check the id is text
-   return false if Mentee[id].nil? # check the database has a record with this id
-
-   true # all checks are ok - the id exists
+   if id.nil? || !Validation.str_is_text?(id) || Mentee[id].nil?
+     return false
+   end
+   true
   end
 
   #Function called when logging in
