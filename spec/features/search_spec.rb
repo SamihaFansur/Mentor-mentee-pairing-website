50fc724a13e55af406fefc9f8549e517299b3816
Euan Goodbrand <egoodbrand1@sheffield.ac.uk>
2021-05-09 15:57:30 +0000

    added test for searching paried users by faculty

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -1,8 +1,8 @@
 require_relative "../spec_helper"
 
-#Tests the search features on the website
+#tests the search features on the website
 
-#Tests the mentor search when it is empty
+#tests the mentor search when it is empty
 describe "the search page" do
   context "with an empty database" do
     it "says the database is empty" do
@@ -14,8 +14,31 @@ describe "the search page" do
       expect(page).to have_content "Your search revealed no mentors"
       clear_database  
     end
-      
- #Checks whether mentee applications have been recieved by mentor        
+      #tests that paired users can be searched for by faculty    
+      it "checks the mentor has been sent an application" do
+      add_test_mentor
+      add_test_user
+      login_mentor    
+      login_mentee
+      visit "/search"
+      fill_in "searchForMentor", with: "Accounting and Financial Management (BA)"
+      click_button "Submit"
+      click_button "Send application" 
+      login_mentor  
+      visit "/menteeApplications"
+      click_button "Accept application"
+      login_mentee
+      visit "/MenteeDashboard"
+      click_link "View sent mentor applications"
+      click_button "Match"
+      login_admin
+      click_link "Paired Mentees"
+      select "Faculty of Arts and Humanities", :from => "searchForMentee" 
+      click_button "Submit" 
+      expect(page).to have_content "0"
+      clear_database
+      end   
+ #checks if applciaitons are empty        
    it "checks the mentor has been sent an application" do
  clear_database
       add_test_mentor
@@ -34,7 +57,7 @@ describe "the search page" do
       clear_database
     end   
       
-#Tests the mentor search when there is only one record of a mentor
+#tests the mentor search when there is only
   context "with one record in the database" do
     it "lists the mentors" do     
       clear_database
@@ -47,7 +70,7 @@ describe "the search page" do
       expect(page).to have_content "Sam Mentor"
       clear_database
     end
-#Checks that mentor does not appear when a different course name is entered
+#checks that mentor doesnt appear when wrong course is entered
     it "does not list the mentor when a different courseName is searched for" do
       clear_database 
       add_test_mentor
@@ -60,7 +83,7 @@ describe "the search page" do
       expect(page).to have_content "Your search revealed no mentors"
       clear_database
     end
-#Checks that a mentor appears in the search when their course name is searched for
+#checks that mentor appears when wrong course is entered
     it "lists the mentor when their courseName is searched for" do
       clear_database
       add_test_mentor
@@ -76,7 +99,7 @@ describe "the search page" do
 
 
     
-     #Checks that mentee is able to unsend mentor applications        
+     #checks if applciaitons are empty        
    it "checks that unsend applicaiton is working" do
  clear_database
       add_test_mentor
@@ -96,9 +119,7 @@ describe "the search page" do
       expect(page).to have_content "There are no pending applications"
       
       clear_database
-    end
-      
-      #Checks that the mentor can check for applications sent to them
+    end 
        it "checks the mentor has been sent an application" do
  clear_database
       add_test_mentor
@@ -115,8 +136,10 @@ describe "the search page" do
       visit "/menteeApplications"
       expect(page).to have_content "Application"
       
-      clear_database
-      end         
+      clear_database     
+      end   
+  
+      
     end
   end   
 end
